bc9125d17295540a3dcbc1622d6dedeb2f2a7b4f
20009174 <20009174@LAPTOP-VNB8QFJD>
2021-07-22 14:16:34 +0800

    Test Loan done by Jordan

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -99,7 +99,7 @@ public class ResourceCentreTest {
 	@Test
 	public void testDoLoanChromebook() {
 		//fail("Not yet implemented");
-		// write your code here
+		// write your code here j
 		assertNotNull("Test if there is valid Camcorder arraylist to add to", chromebookList);
 		ResourceCentre.addChromebook(chromebookList, cb1);
 		ResourceCentre.addChromebook(chromebookList, cb2);
